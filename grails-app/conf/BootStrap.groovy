9eab34b6bf947e60382e54c8305bad8cf989c5ea
Nathan Neff <nathan.neff@gmail.com>
2009-08-21 18:15:29 -0500

    Added possible address2 for dummy data.
    Removed explicit dummy data -- let the
    loadDummyContactAndStudents method do the work.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -15,46 +15,6 @@ class BootStrap {
                                   value:1,
                                   isDefault: true,
                                   description:'When entering Students, this program will be the default program they\'re interested in').save()
-       def c0 = new Contact(firstName:'Jack',
-                           lastName:'Sprat',
-                           address1:'103 Smith Street',
-                           address2:'Apartment 1B',
-                           state:'MO',
-                           zipCode:'63153',
-                           emailAddress:'jack.sprat@crazynate.com').save()
-
-       c0.addToPhoneNumbers(new PhoneNumber(label:'Home', 
-                                            phoneNumber:'(314)-123-4567'))
-
-       c0.addToPhoneNumbers(new PhoneNumber(label:'Work', 
-                                            phoneNumber:'(314)-444-5555'))
-       c0.addToStudents(new Student(lastName:'Sprat',
-                                    firstName:'Snarfo'))
-       c0.save(flush:true)
-       def c1 = new Contact(firstName:'Jill',
-                           lastName:'Pill',
-                           address1:'304 Zonkers Ave.',
-                           state:'MO',
-                           zipCode:'63123')
-
-       if (!c1.validate()) {
-           c1.errors.allErrors.each { println it }
-       }
-       c1.save(flush:true)
-       c1.addToPhoneNumbers(new PhoneNumber(label:'Cell', 
-                                            phoneNumber:'(314)-777-1111'))
-       c1.addToStudents(new Student(lastName:'Pill',
-                                    firstName:'Blue'))
-       c1.addToStudents(new Student(lastName:'Pill',
-                                    firstName:'Jagged',
-                                    middleName:'Little'))
-
-
-       // You have to flush, otherwise hibernate doesn't save this doodie
-       // and the findByFirstNameAndLastName flips out in the loadDevPrograms method.
-       // TODO: Find mobetta way of doing this, like setting some kind of "politeToilet:true"
-       // method so that Grails flushes after every use.
-       c1.save(flush:true)
        loadDevPrograms()
 
        100.times {
@@ -78,9 +38,6 @@ class BootStrap {
        def p2 = new Program(description:"Byteworks Mentorship Program",
                              name:"Mentorship Program").save()
 
-       def s = Student.findByFirstNameAndLastName('Jagged', 'Pill')
-       assert s != null
-       s.addToInterests(new Interest(program:p0, active:true))
    }
 
    // Method used to create a dummy contact, student and an interest
@@ -88,6 +45,11 @@ class BootStrap {
    def loadDummyContactAndStudents() {
        def seed = new Random()
        def randAddress = seed.nextInt(1000) 
+       def address2 = ''
+
+       if (randAddress.mod(3) == 0) {
+           address2 = 'Apt. A'
+       }
        def zip = '63' + seed.nextInt(100).toString().padLeft(3, "0")
        def lastName = randomLastName()
        def firstName = randomFirstName()
@@ -96,6 +58,7 @@ class BootStrap {
        def c0 = new Contact(firstName:firstName,
                            lastName:lastName,
                            address1:randAddress.toString() + ' ' + randomStreetName(),
+                           address2:address2,
                            city:'Saint Louis',
                            state:'MO',
                            zipCode:zip,
