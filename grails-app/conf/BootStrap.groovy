a010d1dba074d826120477e4ed9da54c7dbcac81
Nathan Neff <nathan.neff@gmail.com>
2009-08-21 16:37:06 -0500

    Added pdf plugin, and add tons of random contacts to test data.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -56,6 +56,10 @@ class BootStrap {
        // method so that Grails flushes after every use.
        c1.save(flush:true)
        loadDevPrograms()
+
+       100.times {
+           loadDummyContactAndStudents()
+       }
    } 
 
    def loadDevPrograms() {
@@ -77,6 +81,90 @@ class BootStrap {
        def s = Student.findByFirstNameAndLastName('Jagged', 'Pill')
        assert s != null
        s.addToInterests(new Interest(program:p0, active:true))
+   }
+
+   // Method used to create a dummy contact, student and an interest
+   // in a program
+   def loadDummyContactAndStudents() {
+       def seed = new Random()
+       def randAddress = seed.nextInt(1000) 
+       def zip = '63' + seed.nextInt(100).toString().padLeft(3, "0")
+       def lastName = randomLastName()
+       def firstName = randomLastName()
+       // Use an email address for 50% of the people
+       def emailAddress = seed.nextInt(1) == 1 ?: "${firstName}.${lastName}@" + randomEmailServer() + ".com"
+       def c0 = new Contact(firstName:firstName,
+                           lastName:lastName,
+                           address1:randAddress.toString() + ' ' + randomStreetName(),
+                           state:'MO',
+                           zipCode:zip,
+                           emailAddress:emailAddress).save()
+
+       def randPhone = seed.nextInt(10000) - 1
+       randPhone = randPhone.toString().padLeft(4, "0")
+       c0.addToPhoneNumbers(new PhoneNumber(label:'Home', 
+                                            phoneNumber:"(314)-123-$randPhone"))
+
+       randPhone = seed.nextInt(10000) - 1
+       randPhone = randPhone.toString().padLeft(4, "0")
+       c0.addToPhoneNumbers(new PhoneNumber(label:'Work', 
+                                            phoneNumber:"(314)-444-${randPhone}"))
+
+       def numStudents = seed.nextInt(3) + 1
+       numStudents.times {
+           def stud = new Student(lastName:lastName,
+                                   firstName:randomFirstName())
+
+           c0.addToStudents(stud)
+           c0.save(flush:true)
+          
+           // Add a random amount of interests to random programs :-)
+           def programs = Program.findAll().collect { it.id }
+           def availProgs = programs.clone()
+           // Get 0 .. numPrograms -- some students might not have interests
+           def numProgsForStudent = seed.nextInt(programs.size() + 1)
+           numProgsForStudent.times {
+               println availProgs
+               def randomProg = seed.nextInt(availProgs.size())
+               println randomProg
+               def prog = availProgs.remove(randomProg)
+               stud.addToInterests(new Interest(program:Program.get(prog), active:true))
+           }
+       }
+
+   }
+
+   def randomFirstName() {
+       def seed = new Random()
+       def names = ['Bob', 'Jane', 'Dooge', 'Patty', 'Charlie', 'Snoopy', 'Schizoid',
+                    'Nate', 'Dan', 'Mary', 'Adam', 'Chris', 'Theresa', 'Snarf', 'Peter',
+                    'Missie', 'Julie', 'Wong', 'Debbie']
+       return names[seed.nextInt(names.size() - 1)]
+   }
+   
+   def randomEmailServer() {
+       def seed = new Random()
+       def names = ['yahoo', 'aol', 'gmail', 'grails', 'fiddlesticks']
+       return names[seed.nextInt(names.size() - 1)]
+   }
+
+   def randomLastName() {
+       def seed = new Random()
+       def names = ['Neff', 'James', 'Jones', 'Ryholight', 'Klein', 'Jackson',
+                    'Birdcage', 'Lionheart', 'Braveheart', 'Gibson', 'Snippet',
+                    'Black', 'Caldwell', 'Spacely', 'Jetson', 'Flintstone', 'Fudd',
+                    'Zapata', 'Wolfenstein', 'Davis', 'Washington', 'Jefferson', 'Madison',
+                    'Pujols', 'La Russa', 'Dempster', 'Ryan', 'Ramirez', 'Gonzalez',
+                    'Manson', 'Dahmer', 'Link', 'Stephanopolis', 'Rather', 'Walters',
+                    'Klez', 'Klockenhammer', 'Gosling', 'Torvalds', 'Stallman']
+       return names[seed.nextInt(names.size() - 1)]
+   }
+
+   def randomStreetName() {
+       def seed = new Random()
+       def names = ['Neff', 'James', 'Jones', 'Ryholight', 'Klein', 'Jackson', 'Main',
+                    'Oklahoma', 'Virginia', 'Royal', 'Arsenal Boulevard', 'Zipenstein', 'McCausland Avenue']
+       return names[seed.nextInt(names.size() - 1)]
    }
 
 }
