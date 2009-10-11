42ac3a8045e4a7a76935968f2416e3b85a1f0030
Nathan Neff <nathan.neff@gmail.com>
2009-10-11 16:27:49 -0500

    Added Program Service
    Service's nextAvailableClasses method creates
    stubs of classes that belong to a particular program

--- grails-app/services/org/bworks/bworksdb/ProgramService.groovy
+++ grails-app/services/org/bworks/bworksdb/ProgramService.groovy
@@ -4,14 +4,16 @@ class ProgramService {
 
     boolean transactional = true
 
-    def getCallList(id) {
-        def prog = Program.get(id)
-        if (!prog) return null;
-        def interests = prog.interests.findAll { it.active == true }
-        println interests
-        def students = interests.collect { it.student }
-        def contacts = students.collect { it.contact }
+    // Go through Programs's Classes, and create dummy
+    // LessonDates, starting with startDate
+    def nextAvailableClasses(Program p, Date startDate) {
+        def d = startDate
+        def proposedClasses = []
+        p.lessons.each {
+            proposedClasses << new LessonDate(lesson:it, lessonDate:d)
+            d = d + 7
+        }
 
-        return contacts
+        return proposedClasses
     }
 }
---
+++ test/unit/org/bworks/bworksdb/ProgramServiceTests.groovy
@@ -0,0 +1,17 @@
+package org.bworks.bworksdb
+
+import grails.test.*
+
+class ProgramServiceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
