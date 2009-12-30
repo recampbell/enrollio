47d8b74419e7150bdc3a96e8e572474dcc9112ac
Jeff Brown <jeff@jeffandbetsy.net>
2009-12-30 00:19:06 -0600

    use bootstrap environments

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,61 +3,60 @@ import org.bworks.bworksdb.auth.*
 import org.apache.shiro.crypto.hash.Sha1Hash
 
 class BootStrap {
-    def testDataService
+	def testDataService
 
-    def init = { servletContext ->
-        def totalUsers = ShiroUser.count()
-        
-        if (totalUsers == 0) {
-            loadUserRoles()
+	def init = { servletContext ->
+		def totalUsers = ShiroUser.count()
 
-            // loadDevData will load test users, so make sure that
-            // admin user is already created before you call loadDevData
-            loadAdminUser()
-        }
+		if (totalUsers == 0) {
+			loadUserRoles()
 
-        if (grails.util.GrailsUtil.environment == "development") {
-            testDataService.loadDevData()
-        }
-        
-        if (grails.util.GrailsUtil.environment == "test") {
-            testDataService.loadIntegrationTestData()
-        }
+			// loadDevData will load test users, so make sure that
+			// admin user is already created before you call loadDevData
+			loadAdminUser()
+		}
 
-        if (grails.util.GrailsUtil.environment == "production") {
-            testDataService.loadDefaultConfigSettings()
-        }
-    }
+		environments {
+			production {
+				testDataService.loadDefaultConfigSettings()
+			}
+			development {
+				testDataService.loadDevData()
+			}
+			test {
+				testDataService.loadIntegrationTestData()
+			}
+		}
+	}
 
-    def destroy = {
-    }
+	def destroy = {
+	}
 
-    def loadAdminUser() {
+	def loadAdminUser() {
 
-        // Administrator user and role. 
-        def adminRole = ShiroRole.findByName("Administrator")
-        def adminUser = new ShiroUser(username: "admin", 
-            firstName : 'admin',
-            lastName : 'admin',
-            password : 'admin0',
-            passwordConfirm : 'admin0',
-            passwordHash: new Sha1Hash("admin0").toHex()
-        )
-        if (!adminUser.validate()) {
-            println "User didn't validate!"
-            println adminUser.errors.allErrors
-        }
-        else {
-            adminUser.save()
-            new ShiroUserRoleRel(user: adminUser, role: adminRole).save()
-        }
-    }
+		// Administrator user and role. 
+		def adminRole = ShiroRole.findByName("Administrator")
+		def adminUser = new ShiroUser(username: "admin", 
+				firstName : 'admin',
+				lastName : 'admin',
+				password : 'admin0',
+				passwordConfirm : 'admin0',
+				passwordHash: new Sha1Hash("admin0").toHex()
+				)
+		if (!adminUser.validate()) {
+			println "User didn't validate!"
+			println adminUser.errors.allErrors
+		}
+		else {
+			adminUser.save()
+			new ShiroUserRoleRel(user: adminUser, role: adminRole).save()
+		}
+	}
 
-    def loadUserRoles() {
-        // Initialize User role
-        def userRole = new ShiroRole(name: "User").save() 
-        def adminRole = new ShiroRole(name: "Administrator").save() 
-    }
-
+	def loadUserRoles() {
+		// Initialize User role
+		def userRole = new ShiroRole(name: "User").save() 
+		def adminRole = new ShiroRole(name: "Administrator").save() 
+	}
 }
 
