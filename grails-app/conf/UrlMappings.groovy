e98c11b97a33850983ca3d9fecf8d4247a5331b4
Jeff Brown <jeff@jeffandbetsy.net>
2009-12-30 00:53:40 -0600

    more url mapping

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,49 +1,64 @@
 class UrlMappings {
-    static mappings = {
+	static mappings = {
 
-        // admin controller
-        '/admin'(controller: 'admin', action: 'index')  
+		// admin controller
+		'/admin'(controller: 'admin', action: 'index')
 
-        // auth controller
-        '/login'(controller: 'auth', action: 'login')
-        '/logout'(controller: 'auth', action: 'signOut')
-        '/unauthorized' (controller: 'auth', action: 'unauthorized')
-        '/signin'(controller: 'auth', action: 'signIn')
+		// auth controller
+		'/login'(controller: 'auth', action: 'login')
+		'/logout'(controller: 'auth', action: 'signOut')
+		'/unauthorized' (controller: 'auth', action: 'unauthorized')
+		'/signin'(controller: 'auth', action: 'signIn')
 
-        // classSession controller
+		// classSession controller
 		'/classSessions'(controller: 'classSession', action: 'list')
 		"/classSession/$id"(controller: 'classSession', action: 'show')
+		'/createClassLesson'(controller: 'classSession', action: 'create')
 
-        // configSetting controller
+		// configSetting controller
 		'/settings'(controller: 'configSetting', action: 'list')
 
-        // contact controller
-        '/contact'(controller: 'contact', action: 'index')
-        '/contacts'(controller: 'contact', action: 'list')
-        "/contact/$id"(controller: 'contact', action: 'show')
+		// contact controller
+		'/contact'(controller: 'contact', action: 'index')
+		'/contacts'(controller: 'contact', action: 'list')
+		"/contact/$id"(controller: 'contact', action: 'show')
 
-        // help controller
-        "/"(controller:"help", action:'about')
-        '/help'(controller: 'help', action: 'index')
+		// help controller
+		"/"(controller:"help", action:'about')
+		'/help'(controller: 'help', action: 'index')
 
-        // interest controller
+		// home controller
+		'/home'(controller: 'home', action: 'index')
+
+		// interest controller
 		"/interest/$id"(controller: 'interest', action: 'show')
 
-        // program controller
+		// lesson controller
+		"/lesson/$id"(controller: 'lesson', action: 'show')
+		'/createLesson'(controller: 'lesson', action: 'create')
+		'/saveLesson'(controller: 'lesson', action: 'save')
+		'/lessons'(controller: 'lesson', action: 'list')
+
+		// lessonDate controller
+		"/lessionDate/$id"(controller: 'lessonDate', action: 'show')
+
+		// program controller
 		'/programs'(controller: 'program', action: 'list')
 		"/program/$id"(controller: 'program', action: 'show')
+		'/createProgram'(controller: 'program', action: 'create')
+		'/saveProgram'(controller: 'program', action: 'save')
 
-        // shiroUser controller
-        '/createUser'(controller: 'shiroUser', action: 'create')
-        '/users'(controller: 'shiroUser', action: 'list')
-        "/editUser/$id"(controller: 'shiroUser', action: 'edit')
-        "/deleteUser/$id"(controller: 'shiroUser', action: 'delete')
+		// shiroUser controller
+		'/createUser'(controller: 'shiroUser', action: 'create')
+		'/users'(controller: 'shiroUser', action: 'list')
+		"/editUser/$id"(controller: 'shiroUser', action: 'edit')
+		"/deleteUser/$id"(controller: 'shiroUser', action: 'delete')
 
-        // student controller
+		// student controller
 		'/students'(controller: 'student', action: 'list')
 		"/student/$id"(controller: 'student', action: 'show')
 
-        // response codes
-        "500"(view:'/error')
-    }
+		// response codes
+		"500"(view:'/error')
+	}
 }
