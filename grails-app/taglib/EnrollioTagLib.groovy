13b8970d2d91e004884379cb4a905dbe9d2fc4bc
Nathan Neff <nathan.neff@gmail.com>
2010-01-09 11:19:35 -0600

    Created TagLib textField, dateField to DRY up views.

--- grails-app/taglib/EnrollioTagLib.groovy
+++ grails-app/taglib/EnrollioTagLib.groovy
@@ -6,8 +6,69 @@ class EnrollioTagLib {
     // Standard Enrollio date formatting
     def formatDate = { attrs ->
         def d = attrs['date']
-        def output = d ? d.format('MMMM d, yyyy') : ''
-        out << output
+        try {
+            def output = d ? d.format('MMMM d, yyyy') : ''
+            out << output
+        } catch (Exception e) {
+            out << ''
+        }
     }
+
+    // split a CamelCase fieldName like 'birthDate'
+    // and capitalize, and put a colon on the end
+    def formatFieldName(fieldName) {
+        def formattedFieldName
+        if (fieldName) {
+            def words = fieldName.split("(?=[A-Z])")
+            def upperCaseWords = words.findAll({it.length()}).collect {
+                it[0].toUpperCase() + it[1 .. -1].toLowerCase()
+            }
+            formattedFieldName = upperCaseWords.join(' ') + ':'
+        }
+
+        return formattedFieldName
+    }
+
+
+    def textField = { attrs ->
+        def m = attrs['model']
+        def fieldName = attrs['fieldName']
+        def formattedFieldName = formatFieldName(fieldName)
+
+        def output = """
+        <tr class="prop">
+            <td valign="top" class="name">
+            ${formattedFieldName}
+            </td>
+            <td valign="top" class="value">
+                ${fieldValue(bean: m, field:fieldName)}
+            </td>
+        </tr>
+        """
+
+        out << output
+
+    }
+
+    def dateField = { attrs ->
+        def m = attrs['model']
+        def fieldName = attrs['fieldName']
+        def formattedFieldName = formatFieldName(fieldName)
+
+        def output = """
+        <tr class="prop">
+            <td valign="top" class="name">
+            ${formattedFieldName}
+            </td>
+            <td valign="top" class="value">
+                ${enrollio.formatDate(date:m.properties[fieldName])}
+            </td>
+        </tr>
+        """
+
+        out << output
+
+    }
+
 }
 
