f07c795e4b1d64981477be3e507d53c49b5a55a1
Nathan Neff <nate@lemmy2.gateway.2wire.net>
2010-03-25 22:46:37 -0500

    Load multiple class sessions, and accompanying tests.

--- grails-app/services/org/bworks/bworksdb/DataLoadingService.groovy
+++ grails-app/services/org/bworks/bworksdb/DataLoadingService.groovy
@@ -15,8 +15,6 @@ class DataLoadingService {
             // Date format looks like this:  
             //     <Class1Date>2006-03-11T00:00:00</Class1Date>
             def sessionName = xmlSess.Class1Date.text().split('T')[0]
-            println "xml Session Name is: " + xmlSess.Class1Date.text()
-            println "Session Name is: " + sessionName
             def cs = new ClassSession(course:course, name:sessionName, startDate:new Date())
             if (cs.validate() && cs.save()) {
                 log.info("Imported class session ${cs.name} id: ${cs.id}")
--- test/integration/org/bworks/bworksdb/DataLoadingServiceIntegrationTests.groovy
+++ test/integration/org/bworks/bworksdb/DataLoadingServiceIntegrationTests.groovy
@@ -11,7 +11,7 @@ class DataLoadingServiceIntegrationTests extends GrailsUnitTestCase {
         def existingSession = ClassSession.findByNameIlike("%2006-03-11%")
         assertNull "Should not be a class session", existingSession
 
-        def xml = fixtureClassSession()
+        def xml = fixtureSingleClassSession()
         dataLoadingService.loadClassSessions(xml)
 
         assertEquals "One more class session should now exist.", 1 + initClassSessionCount, ClassSession.count()
@@ -19,14 +19,29 @@ class DataLoadingServiceIntegrationTests extends GrailsUnitTestCase {
         existingSession = ClassSession.findByNameIlike("%2006-03-11%")
         assertNotNull "Class session should have been saved.", existingSession
 
-        assertTrue "Class session name should have first class session date in it",
-                   (existingSession.name =~ /2006-03-11/).matches()
-
-                     
     }
 
+    void testLoadMultipleClassSessions() {
+        def initClassSessionCount = ClassSession.count()
 
-    def fixtureClassSession() {
+        def existingSession = ClassSession.findByNameIlike("%2006-09-09%")
+        assertNull "Should not be a 09-09 class session", existingSession
+
+        existingSession = ClassSession.findByNameIlike("%2006-07-22%")
+        assertNull "Should not be a 7-22 class session either", existingSession
+
+        def xml = fixtureMultipleClassSessions()
+        dataLoadingService.loadClassSessions(xml)
+
+        assertEquals "Four more class sessions should now exist.", 4 + initClassSessionCount, ClassSession.count()
+
+        assertNotNull "Should have saved 09-09 class session", ClassSession.findByNameIlike("%2006-09-09%")
+
+        assertNotNull "Should have saved 7-22 class session also", ClassSession.findByNameIlike("%2006-07-22%")
+
+    }
+
+    def fixtureSingleClassSession() {
         def xml = '''<?xml version="1.0" encoding="UTF-8"?>
 <dataroot xmlns:od="urn:schemas-microsoft-com:officedata" xmlns:xsi="http://www.w3.org/2000/10/XMLSchema-instance"  xsi:noNamespaceSchemaLocation="Class.xsd">
 <Class>
@@ -44,6 +59,60 @@ class DataLoadingServiceIntegrationTests extends GrailsUnitTestCase {
 '''
         return xml
     }
+
+
+    def fixtureMultipleClassSessions() {
+        def xml = '''<?xml version="1.0" encoding="UTF-8"?>
+<dataroot xmlns:od="urn:schemas-microsoft-com:officedata" xmlns:xsi="http://www.w3.org/2000/10/XMLSchema-instance"  xsi:noNamespaceSchemaLocation="Class.xsd">
+<Class>
+<ClassID>1</ClassID>
+<Class1Date>2006-03-11T00:00:00</Class1Date>
+<Class2Date>2006-03-18T00:00:00</Class2Date>
+<Class3Date>2006-03-25T00:00:00</Class3Date>
+<Class4Date>2006-04-01T00:00:00</Class4Date>
+<Class5Date>2006-07-22T00:00:00</Class5Date>
+<Class6Date>2006-08-26T00:00:00</Class6Date>
+<ClassStartTime>11:00 am</ClassStartTime>
+<IsActive>0</IsActive>
+</Class>
+<Class>
+<ClassID>4</ClassID>
+<Class1Date>2006-06-03T00:00:00</Class1Date>
+<Class2Date>2006-06-10T00:00:00</Class2Date>
+<Class3Date>2006-06-17T00:00:00</Class3Date>
+<Class4Date>2006-06-24T00:00:00</Class4Date>
+<Class5Date>2006-07-01T00:00:00</Class5Date>
+<Class6Date>2006-07-08T00:00:00</Class6Date>
+<ClassStartTime>11:00 am</ClassStartTime>
+<IsActive>0</IsActive>
+</Class>
+<Class>
+<ClassID>5</ClassID>
+<Class1Date>2006-07-22T00:00:00</Class1Date>
+<Class2Date>2006-07-29T00:00:00</Class2Date>
+<Class3Date>2006-08-05T00:00:00</Class3Date>
+<Class4Date>2006-08-12T00:00:00</Class4Date>
+<Class5Date>2006-08-19T00:00:00</Class5Date>
+<Class6Date>2006-08-26T00:00:00</Class6Date>
+<ClassStartTime>11:00 am</ClassStartTime>
+<IsActive>0</IsActive>
+</Class>
+<Class>
+<ClassID>7</ClassID>
+<Class1Date>2006-09-09T00:00:00</Class1Date>
+<Class2Date>2006-09-16T00:00:00</Class2Date>
+<Class3Date>2006-09-23T00:00:00</Class3Date>
+<Class4Date>2006-09-30T00:00:00</Class4Date>
+<Class5Date>2006-10-07T00:00:00</Class5Date>
+<Class6Date>2006-10-14T00:00:00</Class6Date>
+<ClassStartTime>11:00 am</ClassStartTime>
+<IsActive>0</IsActive>
+</Class>
+</dataroot>
+'''
+        return xml
+    }
+
 
 }
 
