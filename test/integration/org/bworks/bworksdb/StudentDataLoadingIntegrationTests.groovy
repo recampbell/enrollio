95db9af5996c58443c4bf5fb7f2104e2c623b839
Nathan Neff <nate@lemmy2.gateway.2wire.net>
2010-03-28 10:53:45 -0500

    Put comments about data alongside XML fixtures
    Makes it easier to find test subjects.

--- test/integration/org/bworks/bworksdb/StudentDataLoadingIntegrationTests.groovy
+++ test/integration/org/bworks/bworksdb/StudentDataLoadingIntegrationTests.groovy
@@ -112,29 +112,33 @@ class StudentDataLoadingIntegrationTests extends GrailsUnitTestCase {
 
     def fixtureMultipleStudents() {
         def xml = '''<?xml version="1.0" encoding="UTF-8"?>
-<dataroot xmlns:od="urn:schemas-microsoft-com:officedata" xmlns:xsi="http://www.w3.org/2000/10/XMLSchema-instance"  xsi:noNamespaceSchemaLocation="Student.xsd">
-<Student> <StudentID>188</StudentID> <ParentID>8675309</ParentID> <LastName>Bandit</LastName> <FirstName>Smokey</FirstName> <Notes>Mentorship Interested</Notes>
+<dataroot xmlns:od="urn:schemas-microsoft-com:officedata" xmlns:xsi="http://www.w3.org/2000/10/XMLSchema-instance"  
+xsi:noNamespaceSchemaLocation="Student.xsd">
+'''
+
+        // Smokey Bandit, dropped out, Parent is Tortenweasel, ClassID 13
+        xml = xml + '''<Student> <StudentID>188</StudentID> <ParentID>8675309</ParentID> <LastName>Bandit</LastName> <FirstName>Smokey</FirstName> <Notes>Mentorship Interested</Notes>
 <BirthDate>1997-01-07T00:00:00</BirthDate> <Grade>10</Grade> <ClassID>13</ClassID> <GraduateDate>2007-08-18T00:00:00</GraduateDate>
-<DropOut>0</DropOut> <SystemReceivedID>0</SystemReceivedID> <email>smokeybandit@reynolds.com</email>
+<DropOut>1</DropOut> <SystemReceivedID>0</SystemReceivedID> <email>smokeybandit@reynolds.com</email>
 </Student>
-<Student> <StudentID>188</StudentID> <ParentID>1010101</ParentID> <LastName>Tortenweasel</LastName>
+'''
+
+        // Totoro Tortenweasel, Class ID 13, Graduated, Parent Tortenweasel
+        xml = xml + '''<Student> <StudentID>188</StudentID> <ParentID>1010101</ParentID> <LastName>Tortenweasel</LastName>
 <FirstName>Totoro</FirstName> <Gender>M</Gender> <BirthDate>1997-01-07T00:00:00</BirthDate> <Grade>10</Grade>
-<ClassID>13</ClassID> <GraduateDate>2007-08-18T00:00:00</GraduateDate> <DropOut>0</DropOut> <SystemReceivedID>0</SystemReceivedID> <email>totoro@alum.bworks.org</email>
+<ClassID>13</ClassID> <GraduateDate></GraduateDate> <DropOut>0</DropOut> <SystemReceivedID>0</SystemReceivedID> <email>totoro@alum.bworks.org</email>
 </Student>
-<Student>
-<StudentID>188</StudentID>
-<ParentID>1</ParentID>
-<LastName>Nobodys</LastName>
-<FirstName>Iam</FirstName>
-<BirthDate>1996-01-07T00:00:00</BirthDate>
-<Grade>4</Grade>
-<ClassID>1</ClassID>
-<GraduateDate>2007-08-18T00:00:00</GraduateDate>
-<DropOut>0</DropOut>
-<SystemReceivedID>0</SystemReceivedID>
-<email>smokeybandit@reynolds.com</email>
-</Student>
-<Student>
+'''
+
+        // Nobodys Student, Non-existent class ID
+        xml = xml + ''' <Student> <StudentID>188</StudentID> <ParentID>1</ParentID>
+<LastName>Nobodys</LastName> <FirstName>Iam</FirstName>
+<BirthDate>1996-01-07T00:00:00</BirthDate> <Grade>4</Grade> <ClassID>1</ClassID> <GraduateDate>2007-08-18T00:00:00</GraduateDate> <DropOut>0</DropOut>
+<SystemReceivedID>0</SystemReceivedID> <email></email> </Student> <Student>
+'''
+
+        // Billy Tortenweasel, graduated Class 13
+        xml = xml + '''
 <StudentID>188</StudentID>
 <ParentID>1010101</ParentID>
 <LastName>Tortenweasel</LastName>
