7bf48867037e641e65c032564e83bb0ec02703ba
Nathan Neff <nate@lemmy2.stlbyteworks.net>
2010-05-08 14:22:52 -0500

    Sort sessions by date, descending on the show page.

--- grails-app/domain/org/bworks/bworksdb/Course.groovy
+++ grails-app/domain/org/bworks/bworksdb/Course.groovy
@@ -8,6 +8,10 @@ class Course {
     SortedSet lessons
     static hasMany = [ lessons:Lesson, classSessions:ClassSession, interests:Interest ]
     
+    static mapping = {
+        classSessions sort: "startDate", order: 'desc'
+    }
+    
     static constraints = {
         name(nullable:false, blank:false)
         description(nullable:false, blank:false)
