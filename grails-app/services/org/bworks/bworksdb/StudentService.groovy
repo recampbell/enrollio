ea132866c6699b013df5cfe93fee85bdd4b9a279
Nathan Neff <nathan.neff@gmail.com>
2010-05-12 17:07:16 -0500

    Add signup date to saveInterests

--- grails-app/services/org/bworks/bworksdb/StudentService.groovy
+++ grails-app/services/org/bworks/bworksdb/StudentService.groovy
@@ -21,24 +21,41 @@ class StudentService {
     // create a new one
     // or set active to 'selected'
     // Interests is an array of goofy Grails checkboxes
-    def saveInterests(student, interests) {
+    // signupDates is a map of dates, using the Course's ID as keys
+    // Example:  1 : December 12, 2009        2: January 11, 2010
+    // This allows updating the interest dates for the various courses.
+    // i.e. bob signed up for EAC program in Dec, but signed up for Bicycle in May
+    def saveInterests(student, interests, signupDates = null) {
 
         def selectedInterests = buildSelectedIds(interests)
         
         Course.findAll().each { course ->
             def interest = Interest.findWhere(student:student, course:course)
-            def isSelected = selectedInterests.contains(course.id.toString())
+            def courseId = course.id.toString()
+            def isInterested = selectedInterests.contains(courseId)
+                println "Interest signupDates" + signupDates
 
-            if (! interest) {
-                if (isSelected) {
-                    def note = new Note().save()
-                    def newInterest = new Interest(active:true, student:student, course:course, note:note).save()        
-                    course.addToInterests(newInterest)
-                    student.addToInterests(newInterest)
-                }
+            if (interest) {
+                // Interest exists for this course -- set its active
+                // status according to whether the student's interested or not
+                interest.active = isInterested            
             }
-            else {    
-                interest.active = isSelected            
+            else if (isInterested) {
+                // If student is interested, then create a new Interest
+                def note = new Note().save()
+                interest = new Interest(active:true, student:student, course:course,
+                                                     note:note)
+                course.addToInterests(interest)
+                student.addToInterests(interest)
+            }
+
+            // Apply the signup date, if any
+            if (interest && signupDates && signupDates[courseId]) {
+                println "Interest signupDates" + signupDates[courseId]
+                println "before" + interest.signupDate
+                interest.signupDate = signupDates[courseId]
+                interest.save(flush:true)
+                println "after" + interest.signupDate
             }
         }            
     }
--- test/integration/org/bworks/bworksdb/StudentServiceTests.groovy
+++ test/integration/org/bworks/bworksdb/StudentServiceTests.groovy
@@ -14,9 +14,13 @@ class StudentServiceTests extends GrailsUnitTestCase {
         super.tearDown()
     }
 
-    void assertInterestSelection(student, courseName, isSelected) {
+    void assertInterestSelection(student, courseName, isSelected, signupDate = null) {
         def result = student.interests.find { it.course.name == courseName }
         assertEquals isSelected, result.active        
+        // If caller wants us to verify signupDate, then do so.
+        if (signupDate) {
+            assertEquals signupDate, result.signupDate
+        }
     }
     
     void testSaveInterests_NewInterest() {
@@ -73,4 +77,30 @@ class StudentServiceTests extends GrailsUnitTestCase {
         assertInterestSelection(student, TestKeys.PROGRAM_ADULT_AEC, false)
     }
     
+    // Test signup with a date that's in the past
+    void testSaveInterests_SelectAllInterests_WithDate() {
+        // see TestDataService for preloaded data
+        def student = Student.findByLastName(TestKeys.STUDENT)
+        def selections = []
+        
+        def interestDates = [:]
+        def eacId = Course.findByName(TestKeys.PROGRAM_KIDS_AEC).id.toString()
+        interestDates[eacId] = Date.parse("MM/dd/yyyy", "4/20/2010")
+
+        def adultId = Course.findByName(TestKeys.PROGRAM_ADULT_AEC).id.toString()
+        interestDates[adultId] = Date.parse("MM/dd/yyyy", "4/20/2009")
+
+        def mentId = Course.findByName(TestKeys.PROGRAM_MENTORSHIP).id.toString()
+        interestDates[mentId] = Date.parse("MM/dd/yyyy", "5/11/2010")
+
+        selections << eacId
+        selections << adultId
+        selections << mentId
+        
+        studentService.saveInterests(student, selections, interestDates)
+
+        assertInterestSelection(student, TestKeys.PROGRAM_KIDS_AEC, true, interestDates[eacId])
+        assertInterestSelection(student, TestKeys.PROGRAM_ADULT_AEC, true, interestDates[adultId])
+        assertInterestSelection(student, TestKeys.PROGRAM_MENTORSHIP, true, interestDates[mentId])
+    }
 }
