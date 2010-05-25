cbb0c4f791b094fd5df631b548f4a514f562ce15
Nathan Neff <nathan.neff@gmail.com>
2010-05-25 11:56:48 -0500

    Removed systemDefault option from UserSetting.
    We'll just zap the UserSetting if user wants to use the System setting.

--- grails-app/domain/org/bworks/bworksdb/UserSetting.groovy
+++ grails-app/domain/org/bworks/bworksdb/UserSetting.groovy
@@ -5,10 +5,6 @@ class UserSetting {
     String configKey
     String value
 
-    // Users can choose to use the system default, yet
-    // save their settings for later use/toggling later
-    Boolean useSystemDefault = false
-
     static belongsTo = ShiroUser
     static constraints = {
         value(blank:true)
