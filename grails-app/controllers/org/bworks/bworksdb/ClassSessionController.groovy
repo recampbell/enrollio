380f4386b81aa93f683e3e4c98461a7c13683b9a
Nathan Neff <nathan.neff@gmail.com>
2011-01-28 18:34:43 -0600

    Compute attendences whenever we visit a classSession

--- grails-app/controllers/org/bworks/bworksdb/ClassSessionController.groovy
+++ grails-app/controllers/org/bworks/bworksdb/ClassSessionController.groovy
@@ -114,6 +114,7 @@ class ClassSessionController {
         println "Clas ss " + classSessionInstance
 
         def lessonDateInstance = classSessionService.closestLessonDate(classSessionInstance)
+        attendanceService.initializeAttendees(lessonDateInstance)
 
         if(!classSessionInstance) {
             flash.message = "ClassSession not found with id ${params.id}"
